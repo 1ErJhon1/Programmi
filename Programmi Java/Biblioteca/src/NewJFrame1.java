132e59d556b67437e7b25a2c5dcd6b58ed9a9ad3
1ErJhon1 <giannipozzu@gmail.com>
2024-03-08 15:13:15 +0100

    .

--- Programmi Java/Biblioteca/src/NewJFrame1.java
+++ Programmi Java/Biblioteca/src/NewJFrame1.java
@@ -217,10 +217,10 @@ public class NewJFrame1 extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
---
+++ Programmi Java/Ciclista/src/Ciclista.java
@@ -0,0 +1,38 @@
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jhon
+ */
+public class Ciclista {
+    String nome;
+    int numero;
+    Date tempo;
+    int pos;
+    long diff;
+
+    public Ciclista(String nome, Date tempo, int numero) {
+        this.nome = nome;
+        this.tempo = tempo;
+        this.numero = numero;
+    }
+    
+    public String getDiff(Date primo) {
+        diff = tempo.getTime() - primo.getTime();
+        long secondi = TimeUnit.MILLISECONDS.toSeconds(diff);
+        long minuti = TimeUnit.MILLISECONDS.toMinutes(diff);
+        long ore = TimeUnit.MILLISECONDS.toHours(diff);
+        if(diff<0){
+            return "00:00:00";
+        }else
+            return String.format("%02d:%02d:%02d", ore, minuti%60, secondi%60);
+    }
+}
